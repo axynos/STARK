88f8749d831e421a2fe37b51568ebbc35e842958
axynos <silverkruus@gmail.com>
2017-04-21 21:13:13 +0300

    Style & animate window controls

--- app/components/titlebar/WinControlButton.js
+++ app/components/titlebar/WinControlButton.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import styles from './WinControlButton.scss';
 
 class WinControlButton extends Component {
   render() {
-    // TODO rework this to use an image / icon from a font.
-    const icon = (this.props.type === 'CLOSE') ? 'X' : '_';
-
-    // window.currentWindow is defined in app.html along with an apology for doing it this way.
-    const close = () => window.currentWindow.close();
-    const minimize = () => window.currentWindow.minimize();
+    const buttonStyle = `${styles.button}`;
 
     return (
-      <button onClick={() => (this.props.type === 'CLOSE' ? close() : minimize())}>
-        {icon}
+      <button className={buttonStyle} onClick={this.props.onClick}>
+        <i className="material-icons">{this.props.icon}</i>
       </button>
     );
   }
 }
 
 WinControlButton.propTypes = {
-  type: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  icon: PropTypes.string.isRequired
 };
 
 export default WinControlButton;
